ee6a6c34ebaea80923a01abac51f1f7fd3326b4b
Shashidharak89 <shashidharak334@gmail.com>
2025-12-06 18:29:31 +0530

    Routing added using Link tag

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom"
+import { Link, Route, Routes } from "react-router-dom"
 
 import Home from "./components/Home"
 import Test from "./Test/Test"
@@ -7,6 +7,8 @@ function App() {
 
   return (
     <div>
+      <Link to="/">Home</Link>
+      <Link to="/test">Test</Link>  
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/test" element={<Test/>}/>
